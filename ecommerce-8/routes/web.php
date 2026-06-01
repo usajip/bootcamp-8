a136f80307ca34949f551473aa07ad500d6c1ca4
Aji Pamungkas <apn.24p@gmail.com>
2026-06-01 20:52:03 +0700

    update sesi 15

--- ecommerce-8/routes/web.php
+++ ecommerce-8/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('contoh', [App\Http\Controllers\ContohController::class, 'index']
@@ -21,3 +22,15 @@ Route::get('cart', [CartController::class, 'index'])->name('cart');
 Route::get('checkout', function () {
     return "Ini adalah halaman checkout ecommerce-8.";
 })->name('checkout');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
